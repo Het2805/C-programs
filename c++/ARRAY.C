444f21b30e8dc8ed334b77315a3962edd5bffef0
het2805 <hetpaghdal658@gmail.com>
2025-07-31 23:22:39 +0530

    ARRAY

---
+++ c++/ARRAY.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i[10],a;
+clrscr();
+printf("\n Enter the array element");
+for (a=0;a<10;a++)
+{
+printf("\n Enter the value ",a);
+scanf("%d",&a[i]);
+}
+
+
+
+getch();
+}
